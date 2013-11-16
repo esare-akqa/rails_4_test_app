a84b976a6fd8ca4641aed19f23365d1de8cfcf91
Eric Sare <eric.sare@akqa.com>
2013-11-15 16:33:03 -0800

    refactored views to use variables in title
    cleaned up

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,42 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have content 'Sample App'" do
+    it "should have content 'Home'" do
       # # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get static_pages_index_path
+      # visit static_pages_index_path
       # response.status.should be(200)
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+      # visit '/static_pages/home'
+      visit static_pages_home_path
+      page.should have_content('Home')
+    end
+
+    it 'should have the correct title' do
+      visit static_pages_home_path
+      page.should have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "Help page" do
     it "should have content 'Help'" do
-      # # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get static_pages_index_path
-      # response.status.should be(200)
-      visit '/static_pages/help'
+      visit static_pages_help_path
       page.should have_content('Help')
+    end
+
+    it 'should have the correct title' do
+      visit static_pages_help_path
+      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
     it "should have content 'About Us'" do
-      # # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get static_pages_index_path
-      # response.status.should be(200)
-      visit '/static_pages/about'
+      visit static_pages_about_path
       page.should have_content('About Us')
+    end
+
+    it 'should have the correct title' do
+      visit static_pages_about_path
+      page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
