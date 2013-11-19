8a15250de82dd166ce5c433859b94218be31e035
Eric Sare <eric.sare@akqa.com>
2013-11-19 12:08:39 -0800

    updated routes, test, refactored IE shim to partial, use variables for colors in customs scss

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,56 +5,53 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have content 'Home'" do
       # # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # visit static_pages_index_path
-      # response.status.should be(200)
-      # visit '/static_pages/home'
-      visit static_pages_home_path
+      visit home_path
       page.should have_content('Home')
     end
 
     it 'should have the correct title' do
-      visit static_pages_home_path
+      visit home_path
       page.should have_title("#{base_title}")
     end
 
     it 'should not have Home in title' do
-      visit static_pages_home_path
+      visit home_path
       page.should_not have_title("| Home")
     end
   end
 
   describe "Help page" do
     it "should have content 'Help'" do
-      visit static_pages_help_path
+      visit help_path
       page.should have_content('Help')
     end
 
     it 'should have the correct title' do
-      visit static_pages_help_path
+      visit help_path
       page.should have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have content 'About Us'" do
-      visit static_pages_about_path
+      visit about_path
       page.should have_content('About Us')
     end
 
     it 'should have the correct title' do
-      visit static_pages_about_path
+      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have content 'Contact'" do
-      visit static_pages_contact_path
+      visit contact_path
       page.should have_content('Contact')
     end
 
     it 'should have the correct title' do
-      visit static_pages_contact_path
+      visit contact_path
       page.should have_title("#{base_title} | Contact")
     end
   end
