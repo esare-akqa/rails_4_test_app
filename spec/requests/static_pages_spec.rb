22a36c4c5a0636671c4e19fd90cc3b96030befc3
Eric Sare <eric.sare@akqa.com>
2013-11-19 16:44:24 -0800

    updated specs to check for links on layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,58 @@ require 'spec_helper'
 describe "StaticPages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', :text => heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Bonjour B!TC3S!! Welcome to the best Rails app in the universe!!!' }
+    let(:page_title) { '' }
 
-    it { should have_content('best Rails app in the universe') }
-    it { should have_title(full_title) }
+    it_should_behave_like "all static pages"
     it { should_not have_title("| Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it {should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link "About"
+    current_path.should eq(about_path)
+    should have_selector('h1', :text => 'About Us')
+    click_link "Contact"
+    current_path.should eq(contact_path)
+    should have_selector('h1', :text => 'Contact')
+    click_link "Help"
+    current_path.should eq(help_path)
+    should have_selector('h1', :text => 'Help')
+    click_link "sample app"
+    current_path.should eq(root_path)
+    should have_selector('h1', :text => 'Bonjour B!TC3S!! Welcome to the best Rails app in the universe!!!')
   end
 end
