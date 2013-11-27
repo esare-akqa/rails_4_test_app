62be72d974eed14fe702a2a6c47653bfb0d314bc
Eric Sare <eric.sare@akqa.com>
2013-11-26 16:10:33 -0800

    added factory girl

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-describe "User Pages" do
+describe "User pages" do
+
   subject { page }
 
-  describe "Sign up page" do
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "signup page" do
     before { visit signup_path }
-    it { should have_content('Sign up page') }
-    it { should have_title(full_title('Sign Up')) }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
   end
 end
