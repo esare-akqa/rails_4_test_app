40d6b10eadead74f7b0dfc50e36f8e8717830ddc
Eric Sare <eric.sare@akqa.com>
2013-12-03 11:10:39 -0800

    added spec for testing flash variable if it lingers between request

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Authentication" do
   subject { page }
 
-  describe 'signin page' do
+  describe 'signin' do
     before { visit signin_path }
 
     describe 'with invalid information' do
@@ -11,6 +11,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe 'with valid information' do
