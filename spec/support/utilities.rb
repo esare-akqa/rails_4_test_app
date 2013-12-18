0ccbfa1152123a12d661e135d64b2fe897f32d30
Eric Sare <eric.sare@akqa.com>
2013-12-18 15:44:41 -0800

    FINALLYgit add .! FIXED THE STUPID AUTHENTICATION_PAGES_SPEC failing tests and reverted session new partial to old form_for syntax

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,15 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in 'Email',    with: user.name
-  fill_in 'Password', with: user.password
+  fill_in 'email',    with: user.name
+  fill_in 'password', with: user.password
   click_button 'Sign in'
+end
+
+def check_form_values
+  puts page.find('#email')[:type]
+  email = page.find('#email')[:value]
+  puts "email value: #{email}"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
