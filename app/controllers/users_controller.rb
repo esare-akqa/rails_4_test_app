8121528bfaee58b8c8ad67d624fa50abe65a2881
Eric Sare <eric.sare@akqa.com>
2013-12-23 10:24:45 -0800

    added before_action for signed_in_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:update, :edit]
   def new
     @user = User.new
   end
@@ -35,7 +36,11 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: 'Please sign-in' unless signed_in?
+    end
 end
