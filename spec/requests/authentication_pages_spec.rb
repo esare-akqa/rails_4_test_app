afa353ef154b70f1e5e6be5327e7ba504e0a56ec
Eric Sare <eric.sare@akqa.com>
2014-01-02 16:15:07 -0800

    added microposts_controller to run signed_in_user filter, update routes.rb, refactored signed_in_user method out to sessions_helper, wrote test

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -76,9 +76,6 @@ describe "Authentication" do
 
         describe 'visiting user index' do
           before { visit users_path }
-          # before { get users_path }
-          # specify { expect(response).to be_redirect }
-          # it { puts current_url }
           it { should have_title('Sign in') } 
         end
       end
@@ -110,8 +107,20 @@ describe "Authentication" do
             end
           end
         end
-
       end
+
+      describe 'in the Micropost controller' do
+        describe 'submitting to the create action' do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe 'submitting to the destroy action' do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
     end
 
     describe 'as wrong user' do
